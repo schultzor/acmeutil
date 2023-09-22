30e34abe89d83e199de8383c012866300a2d9c70
Chris Schultz <cjs@clsdn.com>
2023-09-22 14:01:10 -0500

    commit_message

--- gitwin/main.go
+++ gitwin/main.go
@@ -2,7 +2,7 @@
 //
 // Usage:
 //
-//	gitwin -path /path/to/gitrepo/root
+//	gitwin /path/to/gitrepo/root
 //
 // Available commands are defined in the commands.go, they can be enumerated by doing
 // a button 2 click on the "Help" command in the gitwin window.
@@ -15,6 +15,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -22,12 +23,6 @@ import (
 )
 
 // [go install .]
-
-func debugf(format string, args ...interface{}) {
-	if _, debug := os.LookupEnv("DEBUG_WIN"); debug {
-		log.Printf(format, args...)
-	}
-}
 
 type handler struct {
 	w    *acme.Win
@@ -37,7 +32,14 @@ type handler struct {
 
 var (
 	branchTemplate string
+	debugLogs      bool
 )
+
+func debugf(format string, args ...interface{}) {
+	if debugLogs {
+		log.Printf(format, args...)
+	}
+}
 
 func tsbranch() string {
 	return time.Now().Format(branchTemplate)
@@ -72,14 +74,17 @@ func (h *handler) Execute(cmd string) bool {
 }
 
 func readLog(h *handler, l *acme.LogReader) {
+	pfx := filepath.Clean(h.path + "/")
 	for {
 		event, err := l.Read()
 		if err != nil {
 			log.Fatal(err)
 		}
-		if event.Name != "" && event.Op == "put" && strings.HasPrefix(event.Name, h.path) && event.Name != h.path {
+		// update the git status output when a file in the repo is put/written by acme
+		if event.Name != "" && event.Op == "put" && strings.HasPrefix(event.Name, pfx) && event.Name != h.path {
 			debugf("readLog handling %v\n", event)
-			// TODO: should send on the channel being listened to by event loop below
+			// no current way to send an event on the internal channel that EventLoop()
+			// uses to dispatch events, so call our Get method directly here :/
 			h.ExecGet("")
 		}
 	}
@@ -87,7 +92,7 @@ func readLog(h *handler, l *acme.LogReader) {
 
 func main() {
 	log.SetPrefix("gitwin ")
-	pwd, err := os.Getwd()
+	repoPath, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -95,10 +100,14 @@ func main() {
 	if brPfx == "" {
 		brPfx = "branch"
 	}
-	repoPath := flag.String("path", pwd, "path to repo root dir")
+	flag.BoolVar(&debugLogs, "debug", false, "true to enable debug logging")
 	flag.StringVar(&branchTemplate, "branchTemplate", brPfx+"-200601021504", "template for default branch names, populated with time.Format")
 	flag.Parse()
-	if err := os.Chdir(*repoPath); err != nil {
+	args := flag.Args()
+	if len(args) > 0 {
+		repoPath = args[0]
+	}
+	if err := os.Chdir(repoPath); err != nil {
 		log.Fatal("error doing chdir to repo:", err)
 	}
 	w, err := acme.New()
@@ -109,9 +118,9 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	w.Name(*repoPath + "/+git")
+	w.Name(repoPath + "/+git")
 	w.Write("tag", []byte("Get Diff Fetch Pull Branches Push Ls Log Help"))
-	h := handler{path: *repoPath, w: w}
+	h := handler{path: repoPath, w: w}
 	h.ExecGet("")
 	go readLog(&h, l)
 	w.EventLoop(&h)
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/schultzor/acmeutil
 
-go 1.18
+go 1.21
 
 require (
 	9fans.net/go v0.0.4
@@ -11,3 +11,4 @@ require (
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	golang.org/x/xerrors v0.0.0-20191011141410-1b5146add898 // indirect
 )
+
